c84bdb53937e68b6bf5d5914bc62f43dcb028ba0
Adarsh-dev-z <adarsh7013a@gmail.com>
2024-10-10 23:11:51 +0530

    add product

---
+++ my-react-app/server/controllers/productController.ts
@@ -0,0 +1,15 @@
+import { Request, Response } from "express";
+import Product from '../models/Product';
+
+module.exports ={
+    addProduct: async(req: Request, res: Response): Promise<void> =>{
+        try{
+            const {name, price, description, category, inStock} = req.body;
+            const newProduct = new Product({name, price, description, category, inStock});
+            await newProduct.save();
+            res.status(200).json(newProduct);
+        } catch(err){
+            res.status(500).json({ error: 'an error occured while adding product'})
+        }
+    }
+}
