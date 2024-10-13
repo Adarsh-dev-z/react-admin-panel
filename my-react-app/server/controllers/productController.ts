59f6cdf630205634100b9298255f6bdfb5765b02
Adarsh-dev-z <adarsh7013a@gmail.com>
2024-10-13 23:30:05 +0530

    getproductByid

--- my-react-app/server/controllers/productController.ts
+++ my-react-app/server/controllers/productController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import Product from '../models/Product';
+import { error } from "console";
 
 module.exports ={
     addProduct: async(req: Request, res: Response): Promise<void> =>{
@@ -20,5 +21,20 @@ module.exports ={
         } catch(err){
             res.status(500).json({ error: 'failed to fetch products' })
         }
+    },
+
+    getProductById: async (req: Request, res: Response): Promise<void> => {
+        try {
+            const product = await Product.findById(req.params.id);
+            if (!product) {
+                res.status(404).json({ message: 'Product not found' });
+                return; 
+            }
+            res.status(200).json(product);
+        } catch (err: unknown) {
+            const errorMessage = err instanceof Error ? err.message : 'Unknown error';
+            res.status(500).json({ error: `Failed to fetch product: ${errorMessage}` });
+        }
     }
+    
 }
