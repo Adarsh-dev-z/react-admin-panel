9f4f1a56412b7d49a2875dc8308bf7cb398b00ed
Adarsh-dev-z <adarsh7013a@gmail.com>
2024-10-14 22:55:17 +0530

    creating productSlice

--- my-react-app/client/src/slices/productSlice.tsx
+++ my-react-app/client/src/slices/productSlice.tsx
@@ -3,6 +3,7 @@ import axios from "axios";
 
 export const addProduct = createAsyncThunk("admin/add-product", async(productData, {rejectWithValue})=>{
     try{
-        const response = await axios.post("http://localhost:3000/api/admin/add-product")
+        const response = await axios.post("http://localhost:3000/api/admin/add-product", productData, { withCredentials:true});
+        return response.data.products
     }
 })
