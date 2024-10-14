11446533b82fbf86bc5a3251657add9359e884ae
Adarsh-dev-z <adarsh7013a@gmail.com>
2024-10-14 22:53:37 +0530

    creating productSlice

--- my-react-app/client/src/slices/productSlice.tsx
+++ my-react-app/client/src/slices/productSlice.tsx
@@ -1,4 +1,8 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
-export const addProduct = createAsyncThunk("admin/add-product", )
+export const addProduct = createAsyncThunk("admin/add-product", async(productData, {rejectWithValue})=>{
+    try{
+        const response = await axios.post("http://localhost:3000/api/admin/add-product")
+    }
+})
