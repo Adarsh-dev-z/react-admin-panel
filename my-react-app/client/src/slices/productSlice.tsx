18d3c273154aad4235c82691d336f423af29cbae
Adarsh-dev-z <adarsh7013a@gmail.com>
2024-10-15 17:29:33 +0530

    product slice

--- my-react-app/client/src/slices/productSlice.tsx
+++ my-react-app/client/src/slices/productSlice.tsx
@@ -1,9 +1,54 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
-export const addProduct = createAsyncThunk("admin/add-product", async(productData, {rejectWithValue})=>{
-    try{
-        const response = await axios.post("http://localhost:3000/api/admin/add-product", productData, { withCredentials:true});
-        return response.data.products
+
+interface Product {
+    _id: string;
+    name: string;
+    price: number;
+    description: string;
+    category: string;
+    inStock: boolean
+}
+
+interface ProductState {
+    products: Product[]
+    product?: Product | null
+    loading: boolean
+    error?: string | null
+}
+
+const initialState: ProductState = {
+    products: [],
+    product: null,
+    loading: false,
+    error: null
+}
+
+export const addProduct = createAsyncThunk<Product, Omit<Product, "_id">>(
+    "products/addProduct", async (productData, { rejectWithValue }) => {
+        try {
+            const response = await axios.post("http://localhost:3000/api/admin/add-product", productData, { withCredentials: true });
+            return response.data
+        }
+        catch (err:any) {
+            return rejectWithValue(err.response.data)
+        }
+    })
+
+export const fetchProducts = createAsyncThunk<Product[]>(
+    "products/fetchProducts",
+     async(_, {rejectWithValue}) =>{
+        try{
+            const response = await axios.get("http://localhost:3000/api/admin/products", { withCredentials: true });
+            return response.data
+        }
+        catch(err: any){
+            return rejectWithValue(err.response.data)
+        }
     }
-})
+)
+
+export const fetchProductById = createAsyncThunk<Product, string>(
+    "products/fetchProductsById"
+)
