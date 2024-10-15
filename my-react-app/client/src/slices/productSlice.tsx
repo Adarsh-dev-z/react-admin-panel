317775d6d9ea1b7fa979885e6bec37e79de9a966
Adarsh-dev-z <adarsh7013a@gmail.com>
2024-10-15 22:33:06 +0530

    product slice updation

--- my-react-app/client/src/slices/productSlice.tsx
+++ my-react-app/client/src/slices/productSlice.tsx
@@ -1,4 +1,4 @@
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk, PayloadAction } from "@reduxjs/toolkit";
 import axios from "axios";
 
 
@@ -53,7 +53,7 @@ export const fetchProductById = createAsyncThunk<Product, string>(
     "products/fetchProductsById",
     async(id, {rejectWithValue}) =>{
         try{
-            const response = await axios.get("http://localhost:3000/api/admin/productById", { withCredentials: true });
+            const response = await axios.get(`http://localhost:3000/api/admin/productById/${id}`, { withCredentials: true });
             return response.data
         }
         catch(err: any){
@@ -66,7 +66,7 @@ export const updateProduct = createAsyncThunk<Product, {id: string; productData:
     "products/updateProduct",
     async({id, productData}, {rejectWithValue}) =>{
         try{
-            const response = await axios.put("http://localhost:3000/api/admin/productById", { withCredentials: true });
+            const response = await axios.put(`http://localhost:3000/api/admin/updateProduct/${id}`, productData, { withCredentials: true });
             return response.data
         }
         catch(err:any){
@@ -74,3 +74,44 @@ export const updateProduct = createAsyncThunk<Product, {id: string; productData:
         }
     }
 )
+
+export const deleteProduct = createAsyncThunk<string, string>(
+    "products/deleteProduct",
+    async(id, {rejectWithValue})=>{
+        try{
+            await axios.delete(`http://localhost:3000/api/admin/updateProduct/${id}`, { withCredentials: true });
+            return id;
+        }
+        catch(err:any){
+            return rejectWithValue(err.response.data)
+        }
+    }
+)
+
+const productSlice = createSlice({
+    name: "products",
+    initialState,
+    reducers:{
+        resetProductState: (state) =>{
+            state.product = null;
+            state.error = null;
+            state.loading = null;
+        },
+    },
+    extraReducers: (builder) =>{
+        builder
+                .addCase(addProduct.pending, (state)=>{
+                    state.loading=true;
+                    state.error=null
+                })
+                .addCase(addProduct.fulfilled, (state, action: PayloadAction<Product>)=>{
+                    state.loading= false;
+                    state.error=null;
+                    state.products.push(action.payload);
+                })
+                .addCase(addProduct.rejected, (state, action: PayloadAction<any>)=>{
+                    state.loading=false;
+                    state.error= action.payload;
+                })
+    }
+})
