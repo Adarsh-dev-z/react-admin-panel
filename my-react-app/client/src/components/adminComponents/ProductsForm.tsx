d256b3a7f09f505ecf576ede40f7e37dfd44429c
Adarsh-dev-z <adarsh7013a@gmail.com>
2024-10-21 23:19:33 +0530

    productManager.tsx

---
+++ my-react-app/client/src/components/adminComponents/ProductsForm.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { useDispatch, useSelector} from "react-redux"
+import { addProduct, updateProduct, deleteProduct } from "../../slices/productSlice";
+
+const ProductFrom:React.FC = () =>{
+
+
+    return(
+        <div></div>
+    )
+}
