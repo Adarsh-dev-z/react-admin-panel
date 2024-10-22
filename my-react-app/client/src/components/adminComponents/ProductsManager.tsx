7922146164d10bae03b25f09600108da15f8b35d
Adarsh-dev-z <adarsh7013a@gmail.com>
2024-10-22 23:30:15 +0530

    productManager

--- my-react-app/client/src/components/adminComponents/ProductsManager.tsx
+++ my-react-app/client/src/components/adminComponents/ProductsManager.tsx
@@ -4,6 +4,13 @@ import { addProduct, updateProduct, deleteProduct } from "../../slices/productSl
 
 const ProductFrom:React.FC = () =>{
 
+    const [formData, setFormData] = useState<Omit<Product, "_id">>({
+        name: "",
+        price: 0,
+        description: "",
+        category: "",
+        inStock: true,
+      });
 
     return(
         <div></div>
